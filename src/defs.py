ff1ae375ba307724b9093625a8f70aabfbad70c0
Alec Deason <alec@tinycountry.com>
2024-09-10 17:55:02 -0700

    make serialization work (ish)

--- src/defs.py
+++ src/defs.py
@@ -1,13 +1,51 @@
+import pickle
+
 from auto_rogue_ai import *
 from auto_rogue_ai.imports.types import *
+
 class Bot:
-    def __init__(self):
-        self.map = None
+    def map(self):
+        pass
+
+    def broadcast(self):
+        pass
+
+    def get_memory(self):
+        pass
+
+    def set_memory(self, m):
+        pass
 
     def __step__(self) -> Command:
-        if self.map is not None:
-            self.map.update()
-        return self.step()
+        try:
+            m = pickle.loads(load_store())
+            self.set_memory(m)
+        except:
+            pass
+
+        if (map := self.map()) is not None:
+            map.update()
+
+        if (b:= self.broadcast()) is not None:
+            (_, a) = actor();
+            for _, creature in visible_creatures():
+                if a.faction == creature.faction:
+                    if creature.broadcast is not None:
+                        try:
+                            other = pickle.loads(creature.broadcast)
+                            b.merge(other)
+                        except:
+                            pass
+
+        command = self.step()
+
+        if (b:= self.broadcast()) is not None:
+            broadcast(pickle.dumps(b))
+
+        if (m := self.get_memory()) is not None:
+            save_store(pickle.dumps(m))
+
+        return command
 
     def step(self) -> Command:
         Command_Nothing()
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use pyo3::{ffi::c_str, prelude::*, types::{PySet, PyDict, PyTuple, PyList}};
+use pyo3::{ffi::c_str, prelude::*, types::{PySet, PyDict, PyBytes, PyTuple, PyList}};
 use serde::{Serialize, Deserialize};
 use indexmap::IndexMap;
 
-use ::client_utils::{LocSet, LocSetIter, LocMap, bindings::{Loc, Command, ActionTarget}, crdt::CrdtContainer, framework::{ExplorableMap, State, Map}};
+use ::client_utils::{LocSet, LocSetIter, LocMap, bindings::{Loc, Command, ActionTarget, save_store, load_store, broadcast}, crdt::{CrdtContainer, ExpiringFWWRegister, ExpiringSet, SizedFWWExpiringSet, CrdtMap, Fww, Lww, Crdt}, framework::{ExplorableMap, State, Map}};
 
 struct PyLocSet<'a>(Bound<'a, PySet>);
 impl <'a> LocSet for PyLocSet<'a> {
@@ -63,6 +63,15 @@ fn astar<'py>(m: &Bound<'py, PyModule>, current_loc: Bound<'py, PyTuple>, goal: 
     Ok(path)
 }
 
+#[pyfunction]
+pub fn convert<'py>(py: Python<'py>) -> PyResult<Option<Bound<'py, PyAny>>> {
+    if let Some(r) = ::client_utils::behaviors::convert() {
+        Ok(Some(rust_command_to_py_command(r, py)?))
+    } else {
+        Ok(None)
+    }
+}
+
 fn rust_action_target_to_py_action_target<'py>(target: ActionTarget, py: Python<'py>) -> PyResult<Bound<'py, PyAny>> {
     let module = py.import_bound("client_utils")?;
     match target {
@@ -72,6 +81,10 @@ fn rust_action_target_to_py_action_target<'py>(target: ActionTarget, py: Python<
             let loc = loc_class.call1((loc.x, loc.y))?;
             target_class.call1((loc,))
         },
+        ActionTarget::Items(items) => {
+            let target_class = module.dict().get_item("ActionTarget_Items")?.unwrap();
+            target_class.call1((items,))
+        }
         _ => unimplemented!()
     }
 }
@@ -91,8 +104,172 @@ fn rust_command_to_py_command<'py>(command: Command, py: Python<'py>) -> PyResul
     }
 }
 
-#[pyclass(name="ExplorableMap")]
+struct PyAnyCmp(Py<PyAny>);
+
+impl std::clone::Clone for PyAnyCmp {
+    fn clone(&self) -> Self {
+        Python::with_gil(|py| {
+            PyAnyCmp(self.0.clone_ref(py))
+        })
+    }
+}
+
+impl std::cmp::Eq for PyAnyCmp {}
+
+impl std::cmp::PartialEq for PyAnyCmp {
+    fn eq(&self, other: &Self) -> bool {
+        Python::with_gil(|py| {
+            self.0.bind(py).eq(other.0.bind(py)).unwrap_or(false)
+        })
+    }
+}
+
+impl std::cmp::Ord for PyAnyCmp {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        Python::with_gil(|py| {
+            self.0.bind(py).compare(other.0.bind(py)).unwrap()
+        })
+    }
+}
+
+impl std::cmp::PartialOrd for PyAnyCmp {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[pyclass(name="ExpiringFWWRegister")]
 #[derive(Default)]
+struct PyExpiringFWWRegister(ExpiringFWWRegister<PyAnyCmp>);
+
+#[pymethods]
+impl PyExpiringFWWRegister {
+    pub fn get<'py>(&self, py: Python<'py>) -> Option<&Bound<'py, PyAny>> {
+        self.0.get().map(|v| v.0.bind(py))
+    }
+
+    pub fn set<'py>(&mut self, now: i64, expires: i64, value: Bound<'py, PyAny>) {
+        self.0.set(now, expires, PyAnyCmp(value.unbind()));
+    }
+
+    pub fn update_expiry<'py>(&mut self, py: Python<'py>, expires: i64) {
+        self.0.update_expiry(expires);
+    }
+
+    pub fn merge<'py>(&mut self, py: Python<'py>, other: &Self) -> PyResult<()> {
+        self.0.merge(&other.0).unwrap();
+        Ok(())
+    }
+
+    pub fn cleanup(&mut self, now: i64) {
+        self.0.cleanup(now);
+    }
+}
+
+
+#[pyclass(name="ExpiringSet")]
+#[derive(Default)]
+struct PyExpiringSet(ExpiringSet<PyAnyCmp>);
+
+#[pymethods]
+impl PyExpiringSet {
+    pub fn insert<'py>(&mut self, value: Bound<'py, PyAny>, expires: i64) {
+        self.0.insert(PyAnyCmp(value.unbind()), expires);
+    }
+
+    pub fn contains<'py>(&mut self, value: Bound<'py, PyAny>) -> bool {
+        self.0.contains(&PyAnyCmp(value.unbind()))
+    }
+
+    pub fn merge<'py>(&mut self, py: Python<'py>, other: &Self) -> PyResult<()> {
+        self.0.merge(&other.0).unwrap();
+        Ok(())
+    }
+
+    pub fn cleanup(&mut self, now: i64) {
+        self.0.cleanup(now);
+    }
+}
+
+
+#[pyclass(name="SizedFWWExpiringSet")]
+struct PySizedFWWExpiringSet(SizedFWWExpiringSet<PyAnyCmp>);
+
+#[pymethods]
+impl PySizedFWWExpiringSet {
+    #[new]
+    pub fn new(size: usize) -> Self {
+        Self(SizedFWWExpiringSet::new(size))
+    }
+
+    pub fn insert<'py>(&mut self, value: Bound<'py, PyAny>, now: i64, expires: i64) {
+        self.0.insert(PyAnyCmp(value.unbind()), now, expires);
+    }
+
+    pub fn contains<'py>(&mut self, value: Bound<'py, PyAny>) -> bool {
+        self.0.contains(&PyAnyCmp(value.unbind()))
+    }
+
+    pub fn merge<'py>(&mut self, py: Python<'py>, other: &Self) -> PyResult<()> {
+        self.0.merge(&other.0).unwrap();
+        Ok(())
+    }
+
+    pub fn cleanup(&mut self, now: i64) {
+        self.0.cleanup(now);
+    }
+}
+
+#[pyclass(name="FwwCrdtMap")]
+#[derive(Default)]
+struct PyFwwCrdtMap(CrdtMap<PyAnyCmp, PyAnyCmp, Fww>);
+
+#[pymethods]
+impl PyFwwCrdtMap {
+    pub fn insert<'py>(&mut self, key: Bound<'py, PyAny>, value: Bound<'py, PyAny>, now: i64) {
+        self.0.insert(PyAnyCmp(key.unbind()), PyAnyCmp(value.unbind()), now);
+    }
+
+    pub fn contains_key<'py>(&mut self, key: Bound<'py, PyAny>) -> bool {
+        self.0.contains_key(&PyAnyCmp(key.unbind()))
+    }
+
+    pub fn merge<'py>(&mut self, py: Python<'py>, other: &Self) -> PyResult<()> {
+        self.0.merge(&other.0).unwrap();
+        Ok(())
+    }
+
+    pub fn cleanup(&mut self, now: i64) {
+        self.0.cleanup(now);
+    }
+}
+
+#[pyclass(name="LwwCrdtMap")]
+#[derive(Default)]
+struct PyLwwCrdtMap(CrdtMap<PyAnyCmp, PyAnyCmp, Lww>);
+
+#[pymethods]
+impl PyLwwCrdtMap {
+    pub fn insert<'py>(&mut self, key: Bound<'py, PyAny>, value: Bound<'py, PyAny>, now: i64) {
+        self.0.insert(PyAnyCmp(key.unbind()), PyAnyCmp(value.unbind()), now);
+    }
+
+    pub fn contains_key<'py>(&mut self, key: Bound<'py, PyAny>) -> bool {
+        self.0.contains_key(&PyAnyCmp(key.unbind()))
+    }
+
+    pub fn merge<'py>(&mut self, py: Python<'py>, other: &Self) -> PyResult<()> {
+        self.0.merge(&other.0).unwrap();
+        Ok(())
+    }
+
+    pub fn cleanup(&mut self, now: i64) {
+        self.0.cleanup(now);
+    }
+}
+
+#[pyclass(module="client_utils", name="ExplorableMap")]
+#[derive(Default, Serialize, Deserialize)]
 struct PyExplorableMap(ExplorableMap);
 
 #[pymethods]
@@ -121,11 +298,49 @@ impl PyExplorableMap {
             Ok(None)
         }
     }
+
+    pub fn nearest<'py>(&mut self, py: Python<'py>, tys: Vec<String>) -> PyResult<Option<Bound<'py, PyAny>>> {
+        if let Some(Loc { x, y }) = self.0.nearest(&tys) {
+            let module = py.import_bound("client_utils")?;
+            let loc_class = module.dict().get_item("Loc")?.unwrap();
+            let loc = loc_class.call1((x, y))?;
+            Ok(Some(loc))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn move_towards<'py>(&mut self, py: Python<'py>, loc: Bound<'py, PyAny>) -> PyResult<Option<Bound<'py, PyAny>>> {
+        let x:i32 = loc.get_item("x")?.extract()?;
+        let y:i32 = loc.get_item("y")?.extract()?;
+        if let Some(r) = self.0.move_towards(Loc { x, y }) {
+            Ok(Some(rust_command_to_py_command(r, py)?))
+        } else {
+            Ok(None)
+        }
+    }
+
+
+    pub fn __getstate__(&self, py: Python) -> PyResult<PyObject> {
+        Ok(PyBytes::new(py, &bincode::serialize(&self.0).unwrap()).to_object(py))
+    }
+
+    pub fn __setstate__(&mut self, py: Python, state: Bound<PyBytes>) -> PyResult<()> {
+        self.0 = bincode::deserialize(state.as_bytes()).unwrap();
+        Ok(())
+    }
 }
 
 #[pymodule(name = "client_utils")]
 fn client_utils(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(astar, module)?)?;
+    module.add_function(wrap_pyfunction!(convert, module)?)?;
+
+    module.add_class::<PyExpiringFWWRegister>()?;
+    module.add_class::<PyExpiringSet>()?;
+    module.add_class::<PySizedFWWExpiringSet>()?;
+    module.add_class::<PyFwwCrdtMap>()?;
+    module.add_class::<PyLwwCrdtMap>()?;
     module.add_class::<PyExplorableMap>()?;
 
     let code = include_str!("defs.py");
